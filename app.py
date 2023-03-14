5716c335f30d89ebe67d961589caa0350351fc2f
sachi36 <108862527+sachi36@users.noreply.github.com>
2023-03-14 13:56:37 +0530

    Update app.py
    Changes to the existing file

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome to anna earth movers!"
 
-@app.route('/how are you')
+@app.route('/jcb')
 def hello():
-    return 'I am good, how about you?'
+    return 'we will provide the good service'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
